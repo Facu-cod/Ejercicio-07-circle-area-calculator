93ccb4c3c673d311551ba56e4a293e8fb8a6d5c0
Facu-cod <developerfacu@gmail.com>
2024-08-25 15:46:20 -0300

    Create circle-area-calculator.py

---
+++ circle-area-calculator.py
@@ -0,0 +1,5 @@
+radio = float(input("Ingresa el radio del círculo para calcular su área: "))
+
+area = 3.1416 * radio**2
+
+print(f"El área del círculo con un radio de {radio} es igual a {area:.2f}.")
